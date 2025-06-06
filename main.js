1826949cda093cd2f3c8bc09040c02f457fb51d3
jowl <135420045+Joel0560@users.noreply.github.com>
2025-06-06 22:04:35 +0800

    Update main.js

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ ScrollReveal().reveal('.home-contact p, .about-content', {origin: 'right' });
 
 /* Typed JS */
 const typed = new Typed ('.multiple-text',{
-    strings: ['Aspiring Developer'],
+    strings: ['Associate Software Engineer'],
     typeSpeed: 70, 
     backSpeed: 70,
     backDelay:1000,
